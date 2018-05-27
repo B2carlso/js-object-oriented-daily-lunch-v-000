4584421ad737eb893b72461ff5bfa076d056aae2
Brad Carlson <b2carlso@yahoo.com>
2018-05-27 14:43:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,21 @@ class Neighborhood {
     this.id = ++neighborhoodId;
 
     store.neighborhoods.push(this)
-    
+  }
+  deliveries() {
+    return store.deliveries.filter(delivery => delivery.neighborhoodId === this.id);
+  }
+
+
+  customers(){
+    return store.customers.filter(customer => customer.neighborhoodId === this.id);
+  }
+
+
+  meals(){
+    let meals = this.deliveries().map(delivery => delivery.meal());
+    return meals.filter(function(meal, index, meals) {
+      return meals.indexOf(meal) === index;
+    });
   }
 }
